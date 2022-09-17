120c0148d51e6eb1ddd599ff9bb43f00e26d2b9b
sarahemaginnis <sarahemaginnis@gmail.com>
2022-09-17 10:47:51 -0500

    Worked through the Walkers portion of Chapter 2; can now Get All Walkers and Get Walkers by Id

--- DogGo/Program.cs
+++ DogGo/Program.cs
@@ -1,7 +1,10 @@
+using DogGo.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IWalkerRepository, WalkerRepository>();
 
 var app = builder.Build();
 
